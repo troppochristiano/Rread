5f81bbfa1686292ca691ee6aeba8dcb66beb322f
Christian <christianbianchi007@gmail.com>
2026-06-10 22:42:17 +0200

    added analitycs tracking and favicon

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <SpeedInsights />
+    <Analytics />
   </StrictMode>,
 )
